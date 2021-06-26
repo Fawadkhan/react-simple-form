d60a4da04d4e3ca8d42f30249774dca83bd074c1
Adeel <fwk@embrace-it.com>
2021-06-26 23:29:49 +0500

    first commit

---
+++ src/Card/FormCard.styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    width: 600px;
+    color: black;
+    border: 3px solid 
+`;
